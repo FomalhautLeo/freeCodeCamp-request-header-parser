450d56a63401cd5f4b225862de069322bbb0b9da
FomalhautLeo <fomalhautleo@163.com>
2024-11-13 14:22:36 +0800

    :tada: challenge done!

--- index.js
+++ index.js
@@ -24,6 +24,18 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.use('/api/whoami', function (req, res) {
+  console.log("Method: ", req.method);
+  console.log("  IP: ", req.headers.host);
+  console.log("  Lang: ", req.headers['accept-language']);
+  console.log("  Software: ", req.headers['user-agent']);
+  res.json({
+    ipaddress: req.headers.host,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent']
+  })
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
